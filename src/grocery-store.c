bc434bacde190a952fda44e06af0a86e9002e0e8
ryancundiff <90420703+ryancundiff@users.noreply.github.com>
2025-08-23 00:23:45 -0500

    fix(grocery-store): change type of appleReview and change the print message

--- src/grocery-store.c
+++ src/grocery-store.c
@@ -8,14 +8,14 @@ int main() {
   double applePrice = 1.49;
 
   // What customers rate the apples.
-  float appleReview = 82.5f;
+  double appleReview = 82.5;
   short int appleReviewDisplay = (int)appleReview;
 
   // The location of where we sell the apples.
   const char appleLocation = 'F';
 
   printf(
-    "An apple costs: $%.2f, there are %d in inventory found in section: %c and your customers gave it an average review of %d%%!",
+    "An apple costs: $%.2f, there are %d in stock found in section: %c and your customers gave it an average review of %d%%!",
     applePrice,
     appleQuantity,
     appleLocation,
