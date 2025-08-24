c287b2f9279fd837b3bfcf2fe15eee1f7a3d6179
ryancundiff <90420703+ryancundiff@users.noreply.github.com>
2025-08-24 00:06:00 -0500

    feat: add format-date program
    The format-date program asks the user to enter a date in MM/DD/YYYY. The program then prints "<month> <day><suffix>, <year>." (e.g., `December 31st, 1969.`).

---
+++ src/format-date.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(void) {
+  int month, day, year;
+
+  printf("Enter date (MM/DD/YYYY): ");
+  scanf("%d/%d/%d", &month, &day, &year);
+
+  if (month < 1 || month > 12) {
+    printf("\nThe month has to be between 1-12.\n");
+    return 1;
+  }
+
+  if (day < 1 || day > 31) {
+    printf("\nThe day has to be between 1-31.\n");
+    return 1;
+  }
+
+  // Print the month
+  switch (month) {
+    case 1: printf("January "); break;
+    case 2: printf("February "); break;
+    case 3: printf("March "); break;
+    case 4: printf("April "); break;
+    case 5: printf("May "); break;
+    case 6: printf("June "); break;
+    case 7: printf("July "); break;
+    case 8: printf("August "); break;
+    case 9: printf("September "); break;
+    case 10: printf("October "); break;
+    case 11: printf("November "); break;
+    case 12: printf("December ");
+  }
+
+  // Print the day
+  printf("%i", day);
+
+  // Print the suffix for a given day
+  if (day == 11 || day == 12 || day == 13) {
+    printf("th, ");
+  } else {
+    switch (day % 10) {
+      case 1: printf("st, "); break;
+      case 2: printf("nd, "); break;
+      case 3: printf("rd, "); break;
+      default: printf("th, ");
+    }
+  }
+
+  // Print the year
+  printf("%i.\n", year);
+  
+  return 0;
+}
